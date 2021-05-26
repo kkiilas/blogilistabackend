ee81a36bd2a8033c31aaca435238966b8e7dfc78
kkiilas <kadri.kiilas@helsinki.fi>
2021-05-26 20:00:19 +0300

    uibuild

--- tests/blog_api.test.js
+++ tests/blog_api.test.js
@@ -1,8 +1,8 @@
-const supertest = require('supertest')
 const mongoose = require('mongoose')
-const helper = require('./test_helper')
+const supertest = require('supertest')
 const app = require('../app')
 const api = supertest(app)
+const helper = require('./test_helper')
 const jwt = require('jsonwebtoken')
 const Blog = require('../models/blog')
 
