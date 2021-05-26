1d5c6435a67f3b647349a763516f1a3085339846
kkiilas <kadri.kiilas@helsinki.fi>
2021-05-26 19:32:17 +0300

    uibuild

--- utils/config.js
+++ utils/config.js
@@ -6,6 +6,9 @@ const MONGODB_URI = process.env.NODE_ENV === 'test'
   ? process.env.TEST_MONGODB_URI
   : process.env.MONGODB_URI
 
+console.log('process.env.NODE_ENV === test', process.env.NODE_ENV === 'test')
+console.log('mongodb_uri', process.env.MONGODB_URI)
+console.log('mongodb_uri', process.env.TEST_MONGODB_URI)
 console.log('mongodb_uri', MONGODB_URI)
 
 module.exports = {
